ca6429ad62fa7e5bd1acd7688dff6e8c764983f6
roshank8s <roshankumar402@gmail.com>
2025-07-01 18:14:32 +0530

    deleted

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-from . import traktop_optimization
-# from . import res_partner_gmap
-from . import updatamain
-from . import sale_order
-from . import user_registration
-from . import field_service_optmization
-from . import unified_route_optimization
